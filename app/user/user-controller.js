da59c3e94b476b6b9b7afea3b5c6fc288218046a
guzman <sistemasguzman@gmail.com>
2016-07-31 02:41:06 -0500

    nuevo comentario user controller desde la rama developer

--- app/user/user-controller.js
+++ app/user/user-controller.js
@@ -6,14 +6,39 @@
             var self = this;
             self.months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'];
             self.days = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+            self.item = '';
+            self.existsItem = -1;
+
             self.addMonth = function (name) {
                 self.months.push(name);
             }
+            //comentario par la rama developer
+
             self.addDay = function (name) {
-                self.days.push(name);
+                if (!name) return;
+
+                var exits = self.existsDay(name);
+                if (!exits) {
+                    self.days.push(name);
+                } else {
+                    self.existsItem = self.days.indexOf(name, 0);
+                }
+                self.name = '';
             }
+
             self.removeDay = function (index) {
+                self.existsItem = -1;
                 self.days.splice(index, 1);
+            }
+            self.existsDay = function (item) {
+                self.item = item;
+                var res = self.days.some(existsItem);
+                return res;
+            }
+
+
+            function existsItem(item, index, array) {
+                return self.item === item;
             }
         });
 })();             
